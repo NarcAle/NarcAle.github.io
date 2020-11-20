78653c180b1058f0450e00fe3f0cb3e651ea2c9c
alessia <alessia.cerami@studio.unibo.it>
2020-11-20 16:53:59 +0100

    contact style

--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -6,7 +6,6 @@
       </div>
 
       <div class="row mt-1">
-
         <div class="col-lg-4">
           <div class="info">
             <div class="address">
@@ -14,21 +13,17 @@
               <h4>{{ $t('row.location') }}:</h4>
               <p>Folimpopoli, FC, 47034</p>
             </div>
-
             <div class="email">
               <i class="bx bx-envelope"></i>
               <h4>Email:</h4>
               <p><a href="mailto:ceramialessia@gmail.com">ceramialessia@gmail.com</a></p>
             </div>
-
             <div class="phone">
               <i class="bx bx-phone"></i>
               <h4>{{ $t('row.cell') }}:</h4>
               <p>3294735359</p>
             </div>
-
           </div>
-
         </div>
 
         <div class="col-lg-8 mt-5 mt-lg-0">
@@ -51,14 +46,10 @@
               <textarea class="form-control" name="message" rows="5" data-rule="required" :data-msg="$t('row.dataMsgMsg')" :placeholder="$t('row.message')"></textarea>
               <div class="validate"></div>
             </div>
-            
             <div class="text-center"><button type="submit">{{ $t('row.sendMsg') }}</button></div>
           </form>
-
         </div>
-
       </div>
-
     </div>
   </section>
 </template>
