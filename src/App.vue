4046f1bf9ce2662ea6a92a899933a9f200cbe7aa
alessia <alessia.cerami@studio.unibo.it>
2020-11-20 15:50:35 +0100

    updated App vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
-    <div class="locale-switcher">
-      <b-select v-model="$i18n.locale" size="sm" style="width:10%">
-        <option value="en">
-          <div>
-            <img class="localeFlag" src="./assets/flags/en.svg" alt="flag"> 
-            <span>{{ $t('locale-switcher.en') }}</span>
-          </div>
-        </option>
-        <option value="it">
-          <img class="localeFlag" src="@/assets/flags/it.svg" alt="flag"> {{ $t('locale-switcher.it')}}
-        </option>
-      </b-select>
-    </div>
+    
     <Main />
     <Footer />
   </div>
@@ -38,18 +26,12 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #2c3e50;
-  .locale-switcher {
-    background-color: #ECF9F7;
-    display: flex;
-    padding: 2%;
-    justify-content: flex-end;
-  }
-}
+<style lang="sass">
+#app 
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: left
+  color: #2c3e50
+  
 </style>
