d3c71e3941e3b6ffaf63eed1fc374d77bde4b1cc
alessia <alessia.cerami@studio.unibo.it>
2020-11-20 17:39:21 +0100

    skill bar

--- src/components/Skills/Skills.vue
+++ src/components/Skills/Skills.vue
@@ -2,121 +2,61 @@
   <section id="skills" class="skills section-bg">
     <div class="container" data-aos="fade-up">
       <div class="section-title">
-        <h2>Skills</h2>
-        <p>During my universitary study, I practiced different languages. Some of them linked to site web and others linked to develop applications.</p>
+        <h2>{{ $t('section-title.skills')}}</h2>
+        <p>{{ $t('section-title.what') }}</p>
       </div>
-
       <div class="row skills-content">
         <div class="col-lg-6">
-          <div v-for="(skill,index) in skills" :key="index">
-            <span class="lang">{{skill.lang}} <i class="val"> {{skill.percent}}%</i></span>
-            <div class="bar">
-              <div class="progress" v-bind:style = "{'background':skill.color, 'width':skill.percent+'%'}"></div>
-            </div>
+          <div class="progress">
+            <SkillBar skill="HTML" level="100" />
+            <!--<span class="skill">HTML <i class="val">100%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>-->
+          </div>
+          <div class="progress">
+            <SkillBar skill="CSS" level="100" />
+          </div>
+          <div class="progress">
+            <SkillBar skill="JavaScript" level="90" />
+          </div>   
+          <div class="progress">
+            <SkillBar skill="Android" level="85" />
+          </div>
+          <div class="progress">
+            <SkillBar skill="Prolog" level="85" />
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="progress">
+            <SkillBar skill="PHP" level="90" />
+          </div>
+          <div class="progress">
+            <SkillBar skill="Java" level="90" />
+          </div>
+          <div class="progress">
+            <SkillBar skill="Scala" level="85" />
+          </div>
+          <div class="progress">
+            <SkillBar skill="Kotlin" level="85" />
           </div>
         </div>
       </div>
     </div>
-      <!--<div class="row skills-content">
-
-        <div class="col-lg-6">
-          <skill-bar barSize="small" skill="CSS" :level="level" />
-          <div class="progress">
-            <span class="skill">HTML <i class="val">100%</i></span>
-
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-
-          <div class="progress">
-            <span class="skill">CSS <i class="val">100%</i></span>
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-
-          <div class="progress">
-            <span class="skill">JavaScript <i class="val">90%</i></span>
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-    
-          <div class="progress">
-            <span class="skill">Android <i class="val">85%</i></span>
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" value="85" aria-valuemin="0" max="100"></div>
-            </div>
-          </div>
-      
-          <div class="progress">
-            <span class="skill">Prolog <i class="val">85%</i></span>
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-        
-        </div>-->
-
-        <!--<div class="col-lg-6">
-
-          <div class="progress">
-            <span class="skill">PHP <i class="val">90%</i></span>
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-
-          <div class="progress">
-            <span class="skill">Java <i class="val">90%</i></span>
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-
-          <div class="progress">
-            <span class="skill">Scala <i class="val">85%</i></span>
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-        
-          <div class="progress">
-            <span class="skill">Kotlin <i class="val">85%</i></span>
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
-
-        </div>
-    
-
-      </div>
-
-    </div>-->
-
-
   </section>
 </template>
 
+
 <script>
-//import { SkillBar } from 'vue-skill-bar'
+import SkillBar from './SkillBar.vue'
 export default {
-  data() {
-    return {
-      skills:[
-        {lang:"HTML5",	percent:90, color:"#0288d1"},
-        {lang:"CSS3", percent:76, color:"#0288d1"},
-        {lang:"JAVASCRIPT", percent:84, color:"#0288d1"},
-        {lang:"MATERIALIZE",	percent:94, color:"#0288d1"},
-        {lang:"VUE",	percent:93, color:"#0288d1"},
-        {lang:"MONGODB", percent:87, color:"#0288d1"}
-      ]
-    }
+  components: {
+    SkillBar
   },
-  /*components: {
-    'skill-bar': SkillBar
-  },*/
 }
 </script>
+
+<style lang="sass">
+@import './SkillBar.sass'
+</style>
