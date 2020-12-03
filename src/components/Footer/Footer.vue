261cfcabc85e0cefb15c4bff339d6fc62c256608
alessia <alessia.cerami@studio.unibo.it>
2020-12-03 17:01:44 +0100

    link into footer and hero

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -3,10 +3,9 @@
     <div class="container">
       <h3>{{ $t('row.name') }}</h3>
       <div class="social-links">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
         <a href="https://www.facebook.com/alessia.cerami.21/" class="facebook"><i class="bx bxl-facebook"></i></a>
         <a href="https://www.instagram.com/narcale/" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="https://www.linkedin.com/in/alessia-cerami-197006100/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a href="https://www.linkedin.com/in/alessia-cerami-197006100" class="linkedin"><i class="bx bxl-linkedin"></i></a>
       </div>
     </div>
   </div>
--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -10,10 +10,9 @@
         <p> {{ $t('hero.im') }} <span class="typed typing"></span></p>
       </vue-typed-js>
       <div class="social-links">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
         <a href="https://www.facebook.com/alessia.cerami.21/" class="facebook"><i class="bx bxl-facebook"></i></a>
         <a href="https://www.instagram.com/narcale/" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="https://www.linkedin.com/in/alessia-cerami-197006100/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a href="https://www.linkedin.com/in/alessia-cerami-197006100" class="linkedin"><i class="bx bxl-linkedin"></i></a>
       </div>
     </div>
   </section>
